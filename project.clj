efc853c78712096fa0a0329deb8e90fcce6fd2e7
omelhoro <igor.fischer154@gmail.com>
2014-09-17 20:24:21 +0200

    Now includes dep for CLJS testing

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [
-                 [org.clojure/clojure "1.6.0"]
+                [org.clojure/clojure "1.6.0"]
 	                [org.clojure/data.json "0.2.5"]
                  [org.clojure/data.csv "0.1.2"]
                  ]
@@ -31,13 +31,14 @@
                                    [domina "1.0.2"]
                                    ]
                     :test-paths ["test/cljs" "target/test-classes/cljs"]
-                    :plugins [ [lein-cljsbuild "1.0.3"][lein-ring "0.8.8"]]
+                    :plugins [ [com.cemerick/clojurescript.test "0.3.1"][lein-cljsbuild "1.0.3"][lein-ring "0.8.8"]]
                     }
              }
 
   :aliases {  "clj-test" ["with-profile","clj", "test"]
             "clj-build" ["with-profile","clj", "uberjar"]
             "cljs-build" ["with-profile","cljs", "cljsbuild","auto"]
+            "cljs-test" ["with-profile","cljs", "cljsbuild","test"]
             "cljs-serve" ["with-profile","cljs", "ring","server"]
             }
 
@@ -48,9 +49,22 @@
               :compiler {
                 :output-to "syll-app.js"
                 :output-dir "static/syllable/out"
-                :optimizations :whitespace
+                :optimizations :simple
                 :source-map "syll-app.js.map"}
-              }]}
+              }
+            {:source-paths ["target/test-classes/cljs/"]
+                :compiler {:output-to "target/cljs/simple.js"
+                :optimizations :simple
+                :pretty-print true}}
+             ]
+            
+         :test-commands {"unittests" ["phantomjs" :runner
+                                     "this.literal_js_was_evaluated=true"
+                                     "target/cljs/simple.js"
+                                          ]
+                         }
+    }
+
 
   :cljx {:builds [{:source-paths ["src/cljx"]
                  :output-path "target/classes/clj"

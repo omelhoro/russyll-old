bc55b71bc79b85509bc4d8b491d25e59268f2df1
omelhoro <igor.fischer154@gmail.com>
2014-10-16 13:00:02 +0200

    New xml-dependencies and indentation

--- project.clj
+++ project.clj
@@ -2,9 +2,11 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [
-                [org.clojure/clojure "1.6.0"]
-	                [org.clojure/data.json "0.2.5"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]
                  [org.clojure/data.csv "0.1.2"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/data.zip "0.1.1"]
                  ]
   :source-paths ["target/classes/clj" "src/clj"]
   :test-paths ["test/clj" "target/test-classes/clj"]
@@ -13,14 +15,14 @@
   :profiles { 
              :dev {
                    :dependencies [
-                        ;[org.clojure/clojurescript "0.0-2268"]
-                       ; [domina "1.0.2"]
-                        ;[compojure "1.1.6"]
-                        ]
-		              :plugins [[com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]]
-		              :hooks [cljx.hooks]
-		              :ring {:handler rooter/handler}
-              }
+                                  ;[org.clojure/clojurescript "0.0-2268"]
+                                  ; [domina "1.0.2"]
+                                  ;[compojure "1.1.6"]
+                                  ]
+                   :plugins [[com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]]
+                   :hooks [cljx.hooks]
+                   :ring {:handler rooter/handler}
+                   }
 
              :cljs {
                     :source-paths ["src/misc/"]
@@ -44,42 +46,42 @@
 
 
   :cljsbuild {
-    :builds [{:id "syll-app"
-              :source-paths ["src/cljs" "target/classes/cljs" ]
-              :compiler {
-                :output-to "syll-app.js"
-                :output-dir "static/syllable/out"
-                :optimizations :simple
-                :source-map "syll-app.js.map"}
+              :builds [{:id "syll-app"
+                        :source-paths ["src/cljs" "target/classes/cljs" ]
+                        :compiler {
+                                   :output-to "syll-app.js"
+                                   :output-dir "static/syllable/out"
+                                   :optimizations :simple
+                                   :source-map "syll-app.js.map"}
+                        }
+                       {:source-paths ["target/test-classes/cljs/"]
+                        :compiler {:output-to "target/cljs/simple.js"
+                                   :optimizations :simple
+                                   :pretty-print true}}
+                       ]
+
+              :test-commands {"unittests" ["phantomjs" :runner
+                                           "this.literal_js_was_evaluated=true"
+                                           "target/cljs/simple.js"
+                                           ]
+                              }
               }
-            {:source-paths ["target/test-classes/cljs/"]
-                :compiler {:output-to "target/cljs/simple.js"
-                :optimizations :simple
-                :pretty-print true}}
-             ]
-            
-         :test-commands {"unittests" ["phantomjs" :runner
-                                     "this.literal_js_was_evaluated=true"
-                                     "target/cljs/simple.js"
-                                          ]
-                         }
-    }
 
 
   :cljx {:builds [{:source-paths ["src/cljx"]
-                 :output-path "target/classes/clj"
-                 :rules :clj}
+                   :output-path "target/classes/clj"
+                   :rules :clj}
 
-                {:source-paths ["src/cljx"]
-                 :output-path "target/classes/cljs"
-                 :rules :cljs}
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/classes/cljs"
+                   :rules :cljs}
 
-                {:source-paths ["test/cljx"]
+                  {:source-paths ["test/cljx"]
                    :output-path "target/test-classes/clj"
                    :rules :clj}
 
                   {:source-paths ["test/cljx"]
                    :output-path "target/test-classes/cljs"
                    :rules :cljs}]}
-  
+
   )

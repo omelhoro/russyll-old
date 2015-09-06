b67bfa17038703b60197f46fd7a820a0dccb9433
omelhoro <igor.fischer154@gmail.com>
2015-09-06 23:27:53 +0200

    prettiyfing

--- src/misc/rooter.clj
+++ src/misc/rooter.clj
@@ -1,11 +1,11 @@
 (ns rooter
   (:use [compojure.core :only [GET defroutes]])
-  (:require [compojure.handler :as handler]
-            [compojure.route :as route]
-            [ring.util.response :as resp])
- ; (:use [compojure.core :all])
-  )
-  (defroutes app-routes
+  (:require [compojure.handler :as handler] [compojure.route :as route] [ring.util.response :as resp])
+  ; (:use [compojure.core :all])
+)
+
+(defroutes
+  app-routes
   ; to serve document root address
   (GET "/" [] (resp/redirect "index.html"))
   ; to serve static pages saved in resources/public directory
@@ -15,5 +15,4 @@
 
 ;; site function creates a handler suitable for a standard website,
 ;; adding a bunch of standard ring middleware to app-route:
-(def handler
-  (handler/site app-routes)) 
+(def handler (handler/site app-routes))

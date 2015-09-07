e5c3e923af3fb18022157290d39a6b1eaefc76b4
omelhoro <igor.fischer154@gmail.com>
2015-09-07 20:51:33 +0200

    reformatting

--- src/clj/analysis.clj
+++ src/clj/analysis.clj
@@ -2,20 +2,29 @@
   (:require [clojure.data.xml :refer [parse]]))
 
 (require '[clojure.java.io :as io])
+
 (require '[clojure.xml :as xml])
+
 (require '[clojure.zip :as zip])
+
 (require '[clojure.data.zip.xml :as zip-xml])
+
 (def root (-> "/home/igor/tmp/4652.xhtml" io/file xml/parse zip/xml-zip))
 
 (def fl (parse (java.io.StringReader. (slurp (clojure.java.io/file "/home/igor/tmp/4652.xhtml")))))
+
 (def zfl (zip/xml-zip fl))
+
 (vector (zip-xml/xml-> zfl :body :p :se :w))
 
-(dorun (map #(vector %1) (zip-xml/xml-> root :body :p :se :w)))
+(dorun (map #(vector %) (zip-xml/xml-> root :body :p :se :w)))
 
-(map #(second (:content %1))  (zip-xml/xml1-> root :body :p :se :w))
+(map #(second (:content %)) (zip-xml/xml1-> root :body :p :se :w))
 
 (def dir (clojure.java.io/file "/home/igor/Data/ruscorpora_1M/texts"))
+
 (def files (file-seq dir))
+
 (def record (parse (java.io.StringReader. (slurp (last files)))))
+
 (:content (first (:content (second (:content record)))))

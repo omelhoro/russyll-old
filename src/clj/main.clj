1c34497858ca959f3fbd1e5e0185962f33ba33d7
omelhoro <igor.fischer154@gmail.com>
2015-09-07 20:51:42 +0200

    reformatting

--- src/clj/main.clj
+++ src/clj/main.clj
@@ -4,14 +4,14 @@
   (:gen-class))
 
 (defn syll-json [string]
- (let [d (json/read-str string)
-       syll-single (memoize syll-single)]
-   (println (json/write-str (into {} (for [k d] [k (syll-single k)]))))))
+  (let [d (json/read-str string)
+        syll-single (memoize syll-single)]
+    (println (json/write-str (into {} (for [k d] [k (syll-single k)]))))))
 
 (defn orpho-json [string]
- (let [d (json/read-str string)]
-   (println (json/write-str (into {} (for [k d] [k (orpho-single k)]))))))
+  (let [d (json/read-str string)]
+    (println (json/write-str (into {} (for [k d] [k (orpho-single k)]))))))
 
 (defn -main  []
-(doseq [line (line-seq (java.io.BufferedReader. *in*))] 
-  (try (syll-json line) (catch Exception e (println (syll-single line))))))
+  (doseq [line (line-seq (java.io.BufferedReader. *in*))]
+    (try (syll-json line) (catch Exception e (println (syll-single line))))))
